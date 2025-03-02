8cf0d6d632cd40e45df898ff0ef6e286dbe103ca
Marindany-02 <hillarymarindany53@gmail.com>
2025-03-02 20:04:29 +0300

    django authentications

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,3 +1,4 @@
+## forms.py (bookshelf)
 from django import forms
 from .models import Book
 
@@ -5,18 +6,6 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'published_date']
-        widgets = {
-            'published_date': forms.DateInput(attrs={'type': 'date'}),
-        }
-        
-    def clean_title(self):
-        title = self.cleaned_data.get('title')
-        if not title:
-            raise forms.ValidationError("Title is required.")
-        return title
 
-    def clean_author(self):
-        author = self.cleaned_data.get('author')
-        if not author:
-            raise forms.ValidationError("Author is required.")
-        return author
+class ExampleForm(forms.Form):
+    example_field = forms.CharField(max_length=100)
