777326cd3a57b09f8c7ff218e5c995ba3acfd030
Marindany-02 <hillarymarindany53@gmail.com>
2025-03-02 19:02:24 +0300

    django authentications

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Book
+
+from advanced_features_and_security.LibraryProject.relationship_app.admin import CustomUserAdmin
+from .models import Book, CustomUser
 from .models import Book
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
     list_filter=('title','author','publication_year')
     search_fields=('title','author')
 admin.site.register(Book,BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
