c485821c593484c8a10fdec654fd0c3c189ea49e
Marindany-02 <hillarymarindany53@gmail.com>
2025-03-02 17:58:03 +0300

    enabling user authentication

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,20 +3,24 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# Create your models here.
+
 class Author(models.Model):
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
-
+    name = models.CharField(max_length=100)
 
 class Book(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
     def __str__(self):
-        return f"{self.title} by {self.author.name}"
+        return self.title
+
 
 
 class Library(models.Model):
