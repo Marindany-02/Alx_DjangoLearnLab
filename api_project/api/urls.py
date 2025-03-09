feaf9e86d29120b5a19ee75aaed29cbf066ed569
Marindany-02 <hillarymarindany53@gmail.com>
2025-03-09 18:27:04 +0300

    API project

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -5,3 +5,17 @@ from .views import BookList
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
 ]
+# api/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+# Initialize the router
+router = DefaultRouter()
+router.register(r'books', BookViewSet, basename='book')
+
+
+urlpatterns = [
+    # Include the router URLs for BookViewSet
+    path('', include(router.urls)),
+]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -9,3 +9,14 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+# api/views.py
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing, creating, updating, and deleting book instances.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
